b7b384edd8274c0fe93621de4273553a1ee89505
Sanika970 <24b0719@iitb.ac.in>
2026-06-10 18:36:44 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> path;
+
+    bool isPalindrome(string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    void backtrack(string &s, int start) {
+        // Reached the end of the string
+        if (start == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        // Try every possible substring
+        for (int end = start; end < s.size(); end++) {
+            if (isPalindrome(s, start, end)) {
+                path.push_back(s.substr(start, end - start + 1));
+                backtrack(s, end + 1);
+                path.pop_back(); // Backtrack
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        backtrack(s, 0);
+        return ans;
+    }
+};
